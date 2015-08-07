e98017ad29fba4f214bdeffce6784c89f4db7f53
Alex Hirsch <w4rh4wk@bluephoenix.at>
2015-08-07 13:21:43 +0200

    disable seeding of updates

--- scripts/disable-automatic-windowsupdate.ps1
+++ scripts/disable-automatic-windowsupdate.ps1
@@ -1,7 +1,9 @@
 #   Description:
 # This script disables automatic downloading and installation of Windows
-# updates via Group Policies.
+# updates via Group Policies. Additinally seeding updates to other computers is
+# disables.
 
+echo "Disable automatic download and installation of Windows updates"
 $reg = @"
 Windows Registry Editor Version 5.00
 
@@ -10,6 +12,18 @@ Windows Registry Editor Version 5.00
 "AUOptions"=dword:00000002
 "ScheduledInstallDay"=dword:00000000
 "ScheduledInstallTime"=dword:00000003
+"@
+$regfile = "$env:windir\Temp\registry.reg"
+$reg | Out-File $regfile
+Start-Process "regedit.exe" -ArgumentList ("/s", "$regfile") -Wait
+rm $regfile
+
+echo "Disable seeding of updates to other computers"
+$reg = @"
+Windows Registry Editor Version 5.00
+
+[HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\DeliveryOptimization]
+"SystemSettingsDownloadMode"=dword:00000003
 "@
 $regfile = "$env:windir\Temp\registry.reg"
 $reg | Out-File $regfile
