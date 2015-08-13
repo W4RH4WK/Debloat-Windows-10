15d73847b7439f77342a1c788957ecd60c218370
Alex Hirsch <w4rh4wk@bluephoenix.at>
2015-08-13 20:54:07 +0200

    convert dark-theme utility to Powershell script

---
+++ scripts/enable-dark-theme.ps1
@@ -0,0 +1,13 @@
+#   Description
+# This script will set registry keys so Applications using the Appx framework
+# will use the dark theme instead of the default light one.
+
+Import-Module -DisableNameChecking $PSScriptRoot\..\lib\reg-helper.psm1
+
+Import-Registry(@"
+[HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize]
+"AppsUseLightTheme"=dword:00000000
+
+[HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize]
+"AppsUseLightTheme"=dword:00000000
+"@)
