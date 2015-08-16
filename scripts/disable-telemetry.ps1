8a01cf3f07004e70e0ff54ce9b03e765459496d7
Alex Hirsch <w4rh4wk@bluephoenix.at>
2015-08-16 20:07:22 +0200

    block telemetry related IP address via firewall
    ref #6

--- scripts/disable-telemetry.ps1
+++ scripts/disable-telemetry.ps1
@@ -4,8 +4,22 @@
 
 Import-Module -DisableNameChecking $PSScriptRoot\..\lib\reg-helper.psm1
 
-echo "Adding telemetry routes to hosts file"
-cat "$PSScriptRoot\..\res\telemetry-hosts.txt" >> "$env:systemroot\System32\drivers\etc\hosts"
+echo "Adding telemetry domains to hosts file"
+$hosts = cat "$PSScriptRoot\..\res\telemetry-hosts.txt"
+[ipaddress[]] $ips = @()
+foreach ($h in $hosts) {
+    try {
+        # store for next part
+        $ips += [ipaddress]$h
+    } catch [System.InvalidCastException] {
+        # can be redirected by hosts
+        echo "0.0.0.0 $h" >> "$env:systemroot\System32\drivers\etc\hosts"
+    }
+}
+
+echo "Adding telemetry ips to firewall"
+New-NetFirewallRule -DisplayName "Block Telemetry IPs" -Direction Outbound `
+    -Action Block -RemoteAddress ([string[]]$ips)
 
 echo "Disabling telemetry via Group Policies"
 Import-Registry(@"
