0d20c081db9fa9b5c6792c52ef55ac1159113b85
Alex Hirsch <w4rh4wk@bluephoenix.at>
2015-08-19 12:43:08 +0200

    retab

--- scripts/disable-searchui.ps1
+++ scripts/disable-searchui.ps1
@@ -6,11 +6,11 @@ taskkill.exe /F /IM "SearchUI.exe"
 
 # try to rename folder while SearchUI is restarting
 foreach ($_ in (0..15)) {
-	if (Test-Path "$env:windir\SystemApps\Microsoft.Windows.Cortana_cw5n1h2txyewy") {
-		mv "$env:windir\SystemApps\Microsoft.Windows.Cortana_cw5n1h2txyewy" `
-			"$env:windir\SystemApps\_Microsoft.Windows.Cortana_cw5n1h2txyewy" `
-			-ErrorAction SilentlyContinue
-	} else {
-		break
-	}
+    if (Test-Path "$env:windir\SystemApps\Microsoft.Windows.Cortana_cw5n1h2txyewy") {
+        mv "$env:windir\SystemApps\Microsoft.Windows.Cortana_cw5n1h2txyewy" `
+            "$env:windir\SystemApps\_Microsoft.Windows.Cortana_cw5n1h2txyewy" `
+            -ErrorAction SilentlyContinue
+    } else {
+        break
+    }
 }
