32f53b5e25f251b7325a3362622c47fc7116d37b
narutards <narutards@hotmail.com>
2015-08-21 08:14:49 +0200

    Update disable_searchfeatures.ps1

--- scripts/disable_searchfeatures.ps1
+++ scripts/disable_searchfeatures.ps1
@@ -1,12 +1,10 @@
 #   Description: 
 # This script will disable certain unwanted startmenu search features
 
+if (-Not (Get-Item -Path 'HKLM:\Software\Policies\Microsoft\Windows\Windows Search\' -ErrorAction SilentlyContinue)) {
+  New-Item -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search\'
+}
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search\' -Name AllowCortana -Value 0
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search\' -Name DisableWebSearch -Value 1
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search\' -Name AllowSearchToUseLocation -Value 0
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search\' -Name ConnectedSearchUseWeb -Value 0
-
-Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\Windows Search\' -Name AllowCortana -Value 0
-Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\Windows Search\' -Name DisableWebSearch -Value 1
-Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\Windows Search\' -Name AllowSearchToUseLocation -Value 0
-Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\Windows Search\' -Name ConnectedSearchUseWeb -Value 0
