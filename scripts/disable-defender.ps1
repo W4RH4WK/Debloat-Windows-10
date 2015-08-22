b159c7452bffbd35f10a4bc489e5f5343828439f
Alex Hirsch <w4rh4wk@bluephoenix.at>
2015-08-22 18:22:24 +0200

    remove reg-import from defender

--- scripts/disable-defender.ps1
+++ scripts/disable-defender.ps1
@@ -1,16 +1,9 @@
 #   Description:
 # This script will disable Windows Defender via Group Policies.
 
-Import-Module -DisableNameChecking $PSScriptRoot\..\lib\reg-helper.psm1
-
 echo "Disabling Windows Defender"
-Import-Registry(@"
-[HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows Defender]
-"DisableAntiSpyware"=dword:00000001
-"DisableRoutinelyTakingAction"=dword:00000001
-
-[HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows Defender\Policy Manager]
-
-[HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows Defender\Real-Time Protection]
-"DisableRealtimeMonitoring"=dword:00000001
-"@)
+mkdir -Force "HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows Defender"
+sp "HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows Defender" "DisableAntiSpyware" 1
+sp "HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows Defender" "DisableRoutinelyTakingAction" 1
+mkdir -Force "HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows Defender\Real-Time Protection"
+sp "HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows Defender\Real-Time Protection" "DisableRealtimeMonitoring" 1
