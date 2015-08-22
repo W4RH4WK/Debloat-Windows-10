35acad991744f0ce3a1106d819c66c06c02d33ef
Alex Hirsch <w4rh4wk@bluephoenix.at>
2015-08-22 18:26:43 +0200

    remove reg-import from new-volume-control

--- scripts/disable-new-volume-control.ps1
+++ scripts/disable-new-volume-control.ps1
@@ -1,10 +1,6 @@
 #   Description:
 # This script restores the old volume slider.
 
-Import-Module -DisableNameChecking $PSScriptRoot\..\lib\reg-helper.psm1
-
 echo "Restoring old volume slider"
-Import-Registry(@"
-[HKEY_LOCAL_MACHINE\Software\Microsoft\Windows NT\CurrentVersion\MTCUVC]
-"EnableMtcUvc"=dword:00000000
-"@)
+mkdir -Force "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\MTCUVC"
+sp "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\MTCUVC" "EnableMtcUvc" 0
