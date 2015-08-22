d45d3511b8e6c6a6b974d61a993dcb5c9a4e4c17
Alex Hirsch <w4rh4wk@bluephoenix.at>
2015-08-22 23:17:38 +0200

    merge UI related scripts

--- scripts/disable-new-volume-control.ps1
+++
@@ -1,6 +0,0 @@
-#   Description:
-# This script restores the old volume slider.
-
-echo "Restoring old volume slider"
-mkdir -Force "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\MTCUVC"
-sp "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\MTCUVC" "EnableMtcUvc" 0
--- scripts/optimize-startmenu.ps1
+++
@@ -1,6 +0,0 @@
-#   Description:
-# This script will do some optimization on the Windows start menu.
-
-echo "Disabling tile push notification"
-mkdir -Force "HKCU:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\PushNotifications"
-sp "HKCU:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\PushNotifications" "NoTileApplicationNotification" 1
