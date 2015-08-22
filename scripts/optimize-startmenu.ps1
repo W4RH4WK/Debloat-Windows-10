9f27df324ea12e87bccc70e168cae8d9a9a92bd8
Alex Hirsch <w4rh4wk@bluephoenix.at>
2015-08-22 18:52:59 +0200

    remove reg-import from optimize startmenu

--- scripts/optimize-startmenu.ps1
+++ scripts/optimize-startmenu.ps1
@@ -1,10 +1,6 @@
 #   Description:
 # This script will do some optimization on the Windows start menu.
 
-Import-Module -DisableNameChecking $PSScriptRoot\..\lib\reg-helper.psm1
-
 echo "Disabling tile push notification"
-Import-Registry(@"
-[HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\PushNotifications]
-"NoTileApplicationNotification"=dword:00000001
-"@)
+mkdir -Force "HKCU:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\PushNotifications"
+sp "HKCU:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\PushNotifications" "NoTileApplicationNotification" 1
