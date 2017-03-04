3b3eb3f5fda2dfd7bd36c788676cfa766a3de519
Tiago Conceição <Tiago_caza@hotmail.com>
2017-03-04 17:48:18 +0000

    Added batch to run ps scripts
    Easy run scripts from CMD without the need to change or disable 'ExecutionPolicy'

---
+++ scripts/disable-services.bat
@@ -0,0 +1,37 @@
+@echo off
+SET DIR=%~dp0%
+:Begin UAC check and Auto-Elevate Permissions
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+echo:
+echo   Requesting Administrative Privileges...
+echo   Press YES in UAC Prompt to Continue
+echo:
+
+
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+
+    "%temp%\getadmin.vbs"
+    exit /B
+
+
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+
+
+PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%~dp0\disable-services.ps1'"
+pause
