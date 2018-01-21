ffdf5ba9c57038158b08d92ec1c8472114499021
robjarsen <29106074+robjarsen@users.noreply.github.com>
2018-01-21 16:21:44 -0500

    Fix indentation on lib\take-own.psm1

--- lib/take-own.psm1
+++ lib/take-own.psm1
@@ -61,44 +61,44 @@ function Takeown-Folder ($path) {
 function Elevate-Privileges {
 	param($Privilege)
 	$Definition = @"
-    using System;
-    using System.Runtime.InteropServices;
+	using System;
+	using System.Runtime.InteropServices;
 
-    public class AdjPriv {
-        [DllImport("advapi32.dll", ExactSpelling = true, SetLastError = true)]
-            internal static extern bool AdjustTokenPrivileges(IntPtr htok, bool disall, ref TokPriv1Luid newst, int len, IntPtr prev, IntPtr rele);
+	public class AdjPriv {
+		[DllImport("advapi32.dll", ExactSpelling = true, SetLastError = true)]
+			internal static extern bool AdjustTokenPrivileges(IntPtr htok, bool disall, ref TokPriv1Luid newst, int len, IntPtr prev, IntPtr rele);
 
-        [DllImport("advapi32.dll", ExactSpelling = true, SetLastError = true)]
-            internal static extern bool OpenProcessToken(IntPtr h, int acc, ref IntPtr phtok);
+		[DllImport("advapi32.dll", ExactSpelling = true, SetLastError = true)]
+			internal static extern bool OpenProcessToken(IntPtr h, int acc, ref IntPtr phtok);
 
-        [DllImport("advapi32.dll", SetLastError = true)]
-            internal static extern bool LookupPrivilegeValue(string host, string name, ref long pluid);
+		[DllImport("advapi32.dll", SetLastError = true)]
+			internal static extern bool LookupPrivilegeValue(string host, string name, ref long pluid);
 
-        [StructLayout(LayoutKind.Sequential, Pack = 1)]
-            internal struct TokPriv1Luid {
-                public int Count;
-                public long Luid;
-                public int Attr;
-            }
+		[StructLayout(LayoutKind.Sequential, Pack = 1)]
+			internal struct TokPriv1Luid {
+				public int Count;
+				public long Luid;
+				public int Attr;
+			}
 
-        internal const int SE_PRIVILEGE_ENABLED = 0x00000002;
-        internal const int TOKEN_QUERY = 0x00000008;
-        internal const int TOKEN_ADJUST_PRIVILEGES = 0x00000020;
+		internal const int SE_PRIVILEGE_ENABLED = 0x00000002;
+		internal const int TOKEN_QUERY = 0x00000008;
+		internal const int TOKEN_ADJUST_PRIVILEGES = 0x00000020;
 
-        public static bool EnablePrivilege(long processHandle, string privilege) {
-            bool retVal;
-            TokPriv1Luid tp;
-            IntPtr hproc = new IntPtr(processHandle);
-            IntPtr htok = IntPtr.Zero;
-            retVal = OpenProcessToken(hproc, TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, ref htok);
-            tp.Count = 1;
-            tp.Luid = 0;
-            tp.Attr = SE_PRIVILEGE_ENABLED;
-            retVal = LookupPrivilegeValue(null, privilege, ref tp.Luid);
-            retVal = AdjustTokenPrivileges(htok, false, ref tp, 0, IntPtr.Zero, IntPtr.Zero);
-            return retVal;
-        }
-    }
+		public static bool EnablePrivilege(long processHandle, string privilege) {
+			bool retVal;
+			TokPriv1Luid tp;
+			IntPtr hproc = new IntPtr(processHandle);
+			IntPtr htok = IntPtr.Zero;
+			retVal = OpenProcessToken(hproc, TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, ref htok);
+			tp.Count = 1;
+			tp.Luid = 0;
+			tp.Attr = SE_PRIVILEGE_ENABLED;
+			retVal = LookupPrivilegeValue(null, privilege, ref tp.Luid);
+			retVal = AdjustTokenPrivileges(htok, false, ref tp, 0, IntPtr.Zero, IntPtr.Zero);
+			return retVal;
+		}
+	}
 "@
 	$ProcessHandle = (Get-Process -Id $pid).Handle
 	$type = Add-Type $definition -Passthru
