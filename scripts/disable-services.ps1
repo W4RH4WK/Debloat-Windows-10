5906e53043a94582e4d0d51a5cf691fe984c7495
robjarsen <29106074+robjarsen@users.noreply.github.com>
2018-01-20 20:38:15 -0500

    Fix comments on disable-services.ps1

--- scripts/disable-services.ps1
+++ scripts/disable-services.ps1
@@ -3,26 +3,26 @@
 # certain services comment out the corresponding lines below.
 
 $services = @(
-	"diagnosticshub.standardcollector.service"							# Microsoft (R) Diagnostics Hub Standard Collector Service
-	"DiagTrack"																							# Diagnostics Tracking Service
-	"dmwappushservice"																			# WAP Push Message Routing Service (see known issues)
-	"HomeGroupListener"																			# HomeGroup Listener
-	"HomeGroupProvider"																			# HomeGroup Provider
-	"lfsvc"																									# Geolocation Service
-	"MapsBroker"																						# Downloaded Maps Manager
-	"NetTcpPortSharing"																			# Net.Tcp Port Sharing Service
-	"RemoteAccess"																					# Routing and Remote Access
-	"RemoteRegistry"																				# Remote Registry
-	"SharedAccess"																					# Internet Connection Sharing (ICS)
-	"TrkWks"																								# Distributed Link Tracking Client
-	"WbioSrvc"																							# Windows Biometric Service
-	#"WlanSvc"																							# WLAN AutoConfig
-	"WMPNetworkSvc"																					# Windows Media Player Network Sharing Service
-	#"wscsvc"																								# Windows Security Center Service
-	#"WSearch"																							# Windows Search
-	"XblAuthManager"																				# Xbox Live Auth Manager
-	"XblGameSave"																						# Xbox Live Game Save Service
-	"XboxNetApiSvc"																					# Xbox Live Networking Service
+	"diagnosticshub.standardcollector.service"      # Microsoft (R) Diagnostics Hub Standard Collector Service
+	"DiagTrack"                                     # Diagnostics Tracking Service
+	"dmwappushservice"                              # WAP Push Message Routing Service (see known issues)
+	"HomeGroupListener"                             # HomeGroup Listener
+	"HomeGroupProvider"                             # HomeGroup Provider
+	"lfsvc"                                         # Geolocation Service
+	"MapsBroker"                                    # Downloaded Maps Manager
+	"NetTcpPortSharing"                             # Net.Tcp Port Sharing Service
+	"RemoteAccess"                                  # Routing and Remote Access
+	"RemoteRegistry"                                # Remote Registry
+	"SharedAccess"                                  # Internet Connection Sharing (ICS)
+	"TrkWks"                                        # Distributed Link Tracking Client
+	"WbioSrvc"                                      # Windows Biometric Service
+	#"WlanSvc"                                      # WLAN AutoConfig
+	"WMPNetworkSvc"                                 # Windows Media Player Network Sharing Service
+	#"wscsvc"                                       # Windows Security Center Service
+	#"WSearch"                                      # Windows Search
+	"XblAuthManager"                                # Xbox Live Auth Manager
+	"XblGameSave"                                   # Xbox Live Game Save Service
+	"XboxNetApiSvc"                                 # Xbox Live Networking Service
 
 	# Services which cannot be disabled
 	#"WdNisSvc"
