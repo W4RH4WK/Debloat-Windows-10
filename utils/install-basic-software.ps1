1eb7e80becc4878ae60ddf4a2e426173c952c2c3
robjarsen <29106074+robjarsen@users.noreply.github.com>
2018-01-20 20:19:27 -0500

    PowerShell Beautifier CS Fixes
    No spaces for indentation, only tabs.

--- utils/install-basic-software.ps1
+++ utils/install-basic-software.ps1
@@ -4,58 +4,58 @@
 # into your default drive's root directory.
 
 $packages = @(
-    "notepadplusplus.install"
-    "peazip.install"
-    #"7zip.install"
-    #"aimp"
-    #"audacity"
-    #"autoit"
-    #"classic-shell"
-    #"filezilla"
-    #"firefox"
-    #"gimp"
-    #"google-chrome-x64"
-    #"imgburn"
-    #"keepass.install"
-    #"paint.net"
-    #"putty"
-    #"python"
-    #"qbittorrent"
-    #"speedcrunch"
-    #"sysinternals"
-    #"thunderbird"
-    #"vlc"
-    #"windirstat"
-    #"wireshark"
+	"notepadplusplus.install"
+	"peazip.install"
+	#"7zip.install"
+	#"aimp"
+	#"audacity"
+	#"autoit"
+	#"classic-shell"
+	#"filezilla"
+	#"firefox"
+	#"gimp"
+	#"google-chrome-x64"
+	#"imgburn"
+	#"keepass.install"
+	#"paint.net"
+	#"putty"
+	#"python"
+	#"qbittorrent"
+	#"speedcrunch"
+	#"sysinternals"
+	#"thunderbird"
+	#"vlc"
+	#"windirstat"
+	#"wireshark"
 )
 
-echo "Setting up Chocolatey software package manager"
+Write-Output "Setting up Chocolatey software package manager"
 Get-PackageProvider -Name chocolatey -Force
 
-echo "Setting up Full Chocolatey Install"
+Write-Output "Setting up Full Chocolatey Install"
 Install-Package -Name Chocolatey -Force -ProviderName chocolatey
-$chocopath = (Get-Package chocolatey | ?{$_.Name -eq "chocolatey"} | Select @{N="Source";E={((($a=($_.Source -split "\\"))[0..($a.length - 2)]) -join "\"),"Tools\chocolateyInstall" -join "\"}} | Select -ExpandProperty Source)
+$chocopath = (Get-Package chocolatey | Where-Object { $_.Name -eq "chocolatey" } | Select-Object @{ N = "Source"; E = { ((($a = ($_.Source -split "\\"))[0..($a.length - 2)]) -join "\"),"Tools\chocolateyInstall" -join "\" } } | Select-Object -ExpandProperty Source)
 & $chocopath "upgrade all -y"
 choco install chocolatey-core.extension --force
 
-echo "Creating daily task to automatically upgrade Chocolatey packages"
+Write-Output "Creating daily task to automatically upgrade Chocolatey packages"
 # adapted from https://blogs.technet.microsoft.com/heyscriptingguy/2013/11/23/using-scheduled-tasks-and-scheduled-jobs-in-powershell/
 $ScheduledJob = @{
-    Name = "Chocolatey Daily Upgrade"
-    ScriptBlock = {choco upgrade all -y}
-    Trigger = New-JobTrigger -Daily -at 2am
-    ScheduledJobOption = New-ScheduledJobOption -RunElevated -MultipleInstancePolicy StopExisting -RequireNetwork
+	Name = "Chocolatey Daily Upgrade"
+	ScriptBlock = { choco upgrade all -y }
+	Trigger = New-JobTrigger -Daily -at 2am
+	ScheduledJobOption = New-ScheduledJobOption -RunElevated -MultipleInstancePolicy StopExisting -RequireNetwork
 }
 Register-ScheduledJob @ScheduledJob
 
-echo "Installing Packages"
-$packages | %{choco install $_ --force -y}
+Write-Output "Installing Packages"
+$packages | ForEach-Object { choco install $_ --force -y }
 
-echo "Installing Sysinternals Utilities to C:\Sysinternals"
+Write-Output "Installing Sysinternals Utilities to C:\Sysinternals"
 $download_uri = "https://download.sysinternals.com/files/SysinternalsSuite.zip"
-$wc = new-object net.webclient
-$wc.DownloadFile($download_uri, "/SysinternalsSuite.zip")
+$wc = New-Object net.webclient
+$wc.DownloadFile($download_uri,"/SysinternalsSuite.zip")
 Add-Type -AssemblyName "system.io.compression.filesystem"
-[io.compression.zipfile]::ExtractToDirectory("/SysinternalsSuite.zip", "/Sysinternals")
-echo "Removing zipfile"
+[io.compression.zipfile]::ExtractToDirectory("/SysinternalsSuite.zip","/Sysinternals")
+Write-Output "Removing zipfile"
 rm "/SysinternalsSuite.zip"
