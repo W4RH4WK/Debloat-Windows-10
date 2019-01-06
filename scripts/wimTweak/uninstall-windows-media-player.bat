ab172dbac74493e9ee8a1952e8f65e4a6ec0d587
jounihh <40239267+jounihh@users.noreply.github.com>
2019-01-06 18:41:14 +0200

    add uninstall WMPNetworkSharingService

--- scripts/wimTweak/uninstall-windows-media-player.bat
+++ scripts/wimTweak/uninstall-windows-media-player.bat
@@ -2,6 +2,7 @@ cd %~dp0
 
 wimTweak /o /c Microsoft-Windows-MediaPlayer /r
 wimTweak /o /c Microsoft-Windows-WindowsMediaPlayer /r
+wimTweak /o /c Microsoft-Windows-WMPNetworkSharingService /r
 
 rem YOU NEED TO REBOOT IN ORDER FOR THE CHANGES TO TAKE EFFECT
 pause
