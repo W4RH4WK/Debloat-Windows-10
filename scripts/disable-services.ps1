758f58a93740fec444e83d500d978f23fcdfdd1d
supmaxi <samrambo117@gmail.com>
2020-04-29 01:46:36 +1000

    Update disable-services.ps1

--- scripts/disable-services.ps1
+++ scripts/disable-services.ps1
@@ -17,11 +17,12 @@ $services = @(
     "TrkWks"                                   # Distributed Link Tracking Client
     "WbioSrvc"                                 # Windows Biometric Service (required for Fingerprint reader / facial detection)
     #"WlanSvc"                                 # WLAN AutoConfig
+    #"NcdAutoSetup"                            # Network Connected Devices Auto-Setup
+    #"bthserv"                                 # Bluetooth Support Service
+    #"BthAvctpSvc"                             # AVCTP service Audio Video Control Transport Protocol service (BT)
+    #"BTAGService"                             # Bluetooth Audio Gateway Service
     "WMPNetworkSvc"                            # Windows Media Player Network Sharing Service
     "wscsvc"                                   # Windows Security Center Service
-    "XblAuthManager"                           # Xbox Live Auth Manager
-    "XblGameSave"                              # Xbox Live Game Save Service
-    "XboxNetApiSvc"                            # Xbox Live Networking Service
     "ndu"                                      # Windows Network Data Usage Monitor
     "SSDPSRV"                                  #SSDP Discovery
     "lltdsvc"                                  #Link-Layer Topology Discovery Mapper
@@ -41,14 +42,78 @@ $services = @(
     "XblAuthManager"                           #Xbox Live Auth Manager
     "XblGameSave"                              #Xbox Live Game Save
     "XboxNetApiSvc"                            #Xbox Net Api
+    "XboxGipSvc"                               #Xbox Accessory Management Service
     "DmEnrollmentSvc"                          #Device Management Enrollment Service
     "RetailDemo"                               #Retail Demo
     "LanmanServer"                             #File Print Network Sharing
     "LanmanWorkstation"                        #Workstation - Remote Server SMB
+    "LanmanServer"                             #Server
     "seclogon"                                 #Secondary Logon
     "fhsvc"                                    #File History Service
     "CscService"                               #Offline Files
     "WbioSrvc"                                 #Windows Biometric Service
+    "Wecsvc"                                   #Windows Event Collector
+    "WpcMonSvc"                                #Parental Controls
+    "WinRM"                                    #Windows Remote Management (WS-Management)
+    "wcncsvc"                                  #Windows Connect Now
+    "wbengine"                                 #Block Level Backup Engine Service
+    "vmicvss"                                  #Hyper-V Volume Shadow Copy Requestor
+    "vmicvmsession"                            #Hyper-V PowerShell Direct Service
+    "vmictimesync"                             #Hyper-V Time Synchronization Service
+    "vmicshutdown"                             #Hyper-V Guest Shutdown Service
+    "vmicrdv"                                  #Hyper-V Remote Desktop Virtualization Service
+    "vmickvpexchange"                          #Hyper-V Data Exchange Service
+    "vmicheartbeat"                            #Hyper-V Heartbeat Service
+    "vmicguestinterface"                       #Hyper-V Guest Service Interface
+    "HvHost"                                   #HV Host Service
+    "AppVClient"                               #Microsoft App-V Client
+    "upnphost"                                 #UPnP Device Host
+    "UevAgentService"                          #User Experience Virtualization Service
+    "TrkWks"                                   #Distributed Link Tracking Client
+    "TapiSrv"                                  #Telephony (API)
+    "swprv"                                    #Microsoft Software Shadow Copy Provider
+    "stisvc"                                   #Windows Image Acquisition (WIA)
+    "SNMPTRAP"                                 #SNMP Trap
+    "smphost"                                  #Microsoft Storage Spaces SMP
+    "SensrSvc"                                 #Sensor Monitoring Service
+    "Sense"                                    #Windows Defender Advanced Threat Protection Service
+    "SEMgrSvc"                                 #Payments and NFC/SE Manager
+    "SDRSVC"                                   #Windows Backup
+    "SCPolicySvc"                              #Smart Card Removal Policy
+    "ScDeviceEnum"                             #Smart Card Device Enumeration Service
+    "SCardSvr"                                 #Smart Card
+    "RasMan"                                   #Remote Access Connection Manager
+    "RasAuto"                                  #Remote Access Auto Connection Manager
+    "PushToInstall"                            #Windows PushToInstall Service
+    "PolicyAgent"                              #IPsec Policy Agent
+    "PhoneSvc"                                 #Phone Service
+    "PeerDistSvc"                              #BranchCache
+    "NVDisplay.ContainerLocalSystem"           #Container service for NVIDIA root features
+    "NvContainerNetworkService"                #Container service for NVIDIA network features
+    "NvContainerLocalSystem"                   #Container service for NVIDIA root features
+    "NgcSvc"                                   #Microsoft Passport
+    "NgcCtnrSvc"                               #Microsoft Passport Container
+    "Netlogon"                                 #Netlogon domain controller services
+    "NaturalAuthentication"                    #Natural Authentication evaluates signals based on time, network, geolocation, bluetooth
+    "MSiSCSI"                                  #Microsoft iSCSI Initiator Service
+    "MapsBroker"                               #Downloaded Maps Manager
+    "lmhosts"                                  #TCP/IP NetBIOS Helper
+    "LicenseManager"                           #Windows License Manager Service
+    "ITBMService"                              #Intel(R) Turbo Boost Max 3.0 Technology Application Service
+    "irmon"                                    #Infrared monitor service
+    "IpxlatCfgSvc"                             #IP Translation Configuration Service from v4 to v6 and vice versa
+    "iphlpsvc"                                 #IP Helper (6to4, ISATAP, Port Proxy, and Teredo), and IP-HTTPS. 
+    "InstallService"                           #Microsoft Store Install Service
+    "IKEEXT"                                   #IKE and AuthIP IPsec Keying Modules
+    "FrameServer"                              #Windows Camera Frame Server
+    "FontCache"                                #Windows Font Cache Service
+    "fhsvc"                                    #File History Service 
+    "embeddedmode"                             #Embedded Mode service enables scenarios related to Background Applications
+    "DsSvc"                                    #Data Sharing Service
+    "CscService"                               #Offline Files
+    "camsvc"                                   #Capability Access Manager Service
+    "Browser"                                  #unknown
+    "BDESVC"                                   #BitLocker Drive Encryption Service
     
     # Services which cannot be disabled
     #"WdNisSvc"
