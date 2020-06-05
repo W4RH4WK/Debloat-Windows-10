36d3666770b3080ba401fe5661ad6a34437acc3d
Alex Hirsch <alex@dps.uibk.ac.at>
2020-06-05 13:20:15 +0200

    Add note to shell experience host script.
    ref #250

--- utils/disable-ShellExperienceHost.bat
+++ utils/disable-ShellExperienceHost.bat
@@ -1,2 +1,5 @@
+rem Running this script can cause various issues with Explorer, WSL, etc.
+rem See https://github.com/W4RH4WK/Debloat-Windows-10/issues/250
+
 taskkill /F /IM ShellExperienceHost.exe
 move "%windir%\SystemApps\ShellExperienceHost_cw5n1h2txyewy" "%windir%\SystemApps\ShellExperienceHost_cw5n1h2txyewy.bak"
