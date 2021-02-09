16721217119c4aa413202eedccb87ee0c7a128f1
Benoît Dubreuil <b.dubreuil@hotmail.com>
2021-02-09 11:54:54 -0500

    Deleted many undesired stuff

--- utils/boot-advanced-startup.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-shutdown /o /r /t 00
--- utils/disable-prefetch-prelaunch.ps1
+++
@@ -1,7 +0,0 @@
-Disable-MMAgent -ApplicationPreLaunch
-#Get-MMAgent
-
-# disable Prefetch
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management\PrefetchParameters" /v EnablePrefetcher /t REG_DWORD /d "0" /f
-# disable (Edge) Prelaunch
-reg add "HKLM\SOFTWARE\Policies\Microsoft\MicrosoftEdge\Main" /v AllowPrelaunch /t REG_DWORD /d "0" /f
