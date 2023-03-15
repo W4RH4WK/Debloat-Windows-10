21d8e7e2bd4f88b2b57c9c55b60d2d60551d8cd8
Teselka <tesla1999savin@gmail.com>
2023-03-15 23:29:12 +0200

    Fix typo

--- scripts/block-telemetry.ps1
+++ scripts/block-telemetry.ps1
@@ -244,10 +244,10 @@ New-NetFirewallRule -DisplayName "Block Telemetry IPs" -Direction Outbound `
 # Block scheduled telemetry tasks
 # See reference: https://answers.microsoft.com/en-us/windows/forum/windows_10-performance/permanently-disabling-windows-compatibility/6bf71583-81b0-4a74-ae2e-8fd73305aad1
 $tasks = @(
-    "\Mircorosft\Windows\Application Experience\Microsoft Compatibility Appraiser"
-    "\Mircorosft\Windows\Application Experience\ProgramDataUpdater"
-    "\Mircorosft\Windows\Application Experience\StartupAppTask"
-    "\Mircorosft\Windows\Application Experience\PcaPathDbTask"
+    "\Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser"
+    "\Microsoft\Windows\Application Experience\ProgramDataUpdater"
+    "\Microsoft\Windows\Application Experience\StartupAppTask"
+    "\Microsoft\Windows\Application Experience\PcaPatchDbTask"
 )
 
 foreach ($task in $tasks) {
