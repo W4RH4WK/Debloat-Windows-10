d87b4275407e2d8953c1c1d17530b0751c0c97bc
Teselka <tesla1999savin@gmail.com>
2023-03-17 12:27:16 +0200

    Block telemetry scheduled tasks (#310)
    * Block telemetry scheduled tasks
    
    * Fix typo

--- scripts/block-telemetry.ps1
+++ scripts/block-telemetry.ps1
@@ -240,3 +240,16 @@ $ips = @(
 Remove-NetFirewallRule -DisplayName "Block Telemetry IPs" -ErrorAction SilentlyContinue
 New-NetFirewallRule -DisplayName "Block Telemetry IPs" -Direction Outbound `
     -Action Block -RemoteAddress ([string[]]$ips)
+
+# Block scheduled telemetry tasks
+# See reference: https://answers.microsoft.com/en-us/windows/forum/windows_10-performance/permanently-disabling-windows-compatibility/6bf71583-81b0-4a74-ae2e-8fd73305aad1
+$tasks = @(
+    "\Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser"
+    "\Microsoft\Windows\Application Experience\ProgramDataUpdater"
+    "\Microsoft\Windows\Application Experience\StartupAppTask"
+    "\Microsoft\Windows\Application Experience\PcaPatchDbTask"
+)
+
+foreach ($task in $tasks) {
+   Disable-ScheduledTask -TaskName $task
+}
